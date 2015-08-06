49a3335fef67ecafbd617f2c740d4f5ee024e1e9
WangMinjun <15895899337@163.com>
2015-08-06 22:49:53 +0800

    Initial Commit

---
+++ MJdeFM-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MJdeFM-Swift
+//
+//  Created by WangMinjun on 15/8/6.
+//  Copyright (c) 2015年 WangMinjun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
